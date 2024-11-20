b9d2c799d022cc897395c94420e3db80be0dc2c0
dongmin-dpsl <dongmin.kim@deepsales.com>
2024-11-20 23:39:29 +0900

    refactor: [#5] user repository 구현

---
+++ src/database/opensearch/user.entity.ts
@@ -0,0 +1,23 @@
+export const UserEntity = {
+  alias: 'user',
+  email: {
+    _: 'email',
+    keyword: 'email.keyword',
+  },
+  first_name: {
+    _: 'first_name',
+    keyword: 'first_name.keyword',
+  },
+  gender: {
+    _: 'gender',
+    keyword: 'gender.keyword',
+  },
+  ip_address: {
+    _: 'ip_address',
+    keyword: 'ip_address.keyword',
+  },
+  last_name: {
+    _: 'last_name',
+    keyword: 'last_name.keyword',
+  },
+};
