01c921b506383da79d62fcbc8e382ff0b5664e07
dongmin-dpsl <dongmin.kim@deepsales.com>
2024-11-20 23:39:51 +0900

    refactor: [#5] 모듈 임폴트

--- src/common/module/user.module.ts
+++ src/common/module/user.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserService } from '../../domain/user.service';
 import { UserController } from '../../controller/user/user.controller';
 import { OpenSearch } from '../../common/lib/opensearch';
+import { UserRepository } from 'src/database/opensearch/user.repository';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, OpenSearch],
+  providers: [
+    OpenSearch,
+    { provide: 'UserUseCase', useClass: UserService },
+    { provide: 'UserOsCommandPort', useClass: UserRepository },
+    { provide: 'UserOsQueryPort', useClass: UserRepository },
+  ],
 })
 export class UserModule {}
