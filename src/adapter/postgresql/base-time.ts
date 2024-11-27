d9beac75cf5a8e02fa81a547f62fbd4c34a3bf87
dogmin-dpsl <dongmin.kim@deepsales.com>
2024-11-27 20:06:41 +0900

    fix: 생성일자 자동생성으로 변경

--- src/adapter/postgresql/base-time.ts
+++ src/adapter/postgresql/base-time.ts
@@ -1,7 +1,7 @@
 import { Property } from '@mikro-orm/core';
 
 export abstract class BaseTimeEntity {
-  @Property({ name: 'created_at' })
+  @Property({ name: 'created_at', onCreate: () => new Date() })
   createdAt = new Date();
 
   @Property({ name: 'updated_at', onUpdate: () => new Date() })
